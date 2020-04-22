00173c6696290f2cc7d0a6fb35fdb0260da780af
Phil <philip.schaefer0@gmail.com>
2020-04-22 21:20:20 +0200

    added commentery

--- functions/FUNC_sharedChannels.js
+++ functions/FUNC_sharedChannels.js
@@ -1,7 +1,15 @@
 const bridgedChannel = require('../database/models/bridgedChannel');
 
 module.exports.run = async (client, message, config) => {
+  // get channel entry with hub name
 
+  // get all channels in hub
+
+  // post message in every channel besides original one
+
+  // log messages in DB
+
+  // check message DB if there are expiered messages
 };
 
 module.exports.help = {
