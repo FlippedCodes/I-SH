dd5d18167138aa0c5cd63e6e920ae1350dab4ba5
Phil <philip.schaefer0@gmail.com>
2020-04-23 23:40:20 +0200

    inital init

---
+++ commands/hub.js
@@ -0,0 +1,29 @@
+// prepares command usage message
+function CommandUsage(prefix, cmdName, subcmd) {
+  return `Command usage: 
+    \`\`\`${prefix}${cmdName} ${subcmd}\`\`\``;
+}
+
+// creates a embed messagetemplate for failed actions
+function messageFail(client, message, body) {
+  client.functions.get('FUNC_richEmbedMessage')
+    .run(client.user, message.channel, body, '', 16449540, false);
+}
+
+module.exports.run = async (client, message, args, config) => {
+  const [subcmd] = args;
+  const commandValues = ['register', 'delete', 'join', 'leave'];
+  if (commandValues.includes(subcmd)) {
+    client.functions.get(`CMD_hub_${subcmd}`)
+      .run(client, message, args, config);
+  } else {
+    const currentCMD = module.exports.help;
+    messageFail(client, message, CommandUsage(config.prefix, currentCMD.name, currentCMD.usage));
+  }
+};
+
+module.exports.help = {
+  name: 'hub',
+  usage: 'register|delete|join|leave',
+  desc: 'More info on every sub-command.',
+};
