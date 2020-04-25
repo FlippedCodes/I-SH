3337063211776e5deff1160c8888875c1002440f
Philip Schäfer <schaefer@bitsteps.local>
2020-04-25 21:39:20 +0200

    added core fuctions

--- functions/CMD_hub_join.js
+++ functions/CMD_hub_join.js
@@ -1,5 +1,17 @@
+// prepares command usage message
+function CommandUsage(prefix, cmdName, subcmd) {
+  return `Command usage: 
+    \`\`\`${prefix}${cmdName} ${subcmd}\`\`\``;
+}
+
+// creates a embed messagetemplate for failed actions
+function messageFail(client, message, body) {
+  client.functions.get('FUNC_richEmbedMessage')
+    .run(client.user, message.channel, body, '', 16449540, false);
+}
+
 module.exports.run = async (client, message, args, config) => {
-  message.reply('jn');
+  
 };
 
 module.exports.help = {
