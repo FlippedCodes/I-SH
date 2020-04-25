87f22fa6dc51338a6c534cd901590b614ff5426d
Phil <schaefer@bitsteps.local>
2020-04-26 01:21:01 +0200

    populated with code

--- functions/EVENT_messageDelete.js
+++ functions/EVENT_messageDelete.js
@@ -1,22 +1,55 @@
+const BridgedChannel = require('../database/models/bridgedChannel');
+
+const MessageLink = require('../database/models/messageLink');
+
+const errHander = (err) => { console.error('ERROR:', err); };
+
+async function checkChannel(channelID) {
+  const result = await BridgedChannel.findOne({ attributes: [], where: { channelID } }).catch(errHander);
+  return result;
+}
+
+async function getMessageInstance(messageID) {
+  const result = await MessageLink.findOne({ attributes: ['messageInstanceID'], where: { messageID } }).catch(errHander);
+  return result;
+}
+
+async function getDBMessages(messageInstanceID) {
+  const result = await MessageLink.find({ attributes: ['messageID'], where: { messageInstanceID } }).catch(errHander);
+  return result;
+}
+
+function deleteDBMessage(messageInstanceID) {
+  MessageLink.destroy({ where: messageInstanceID });
+}
+
+async function getMessages(messageID) {
+  // check DB for messageID and get messageInstanceID
+  const messageInstanceID = await getMessageInstance(messageID);
+  if (!messageInstanceID) return null;
+  // get all messageIDs
+  const allMessageIDs = await getDBMessages(messageInstanceID);
+  deleteDBMessage(messageInstanceID);
+  return allMessageIDs;
+}
+
 // Deletes all messages in every channel, if its deleted in one
 module.exports.run = async (client, message, config) => {
-  // TODO: make messageDelete event
-
-  // get message and channel ID
-
   // check if channel is part of service
+  if (!await checkChannel(message.channel_id)) return;
 
-  // check DB for messageID
+  const allMessageIDs = await getMessages(message.id);
+  if (!allMessageIDs) return;
 
-  // get messageInstanceID
-
-  // get all messageIDs
-
-  // [REQUIERES MESSAGE MANAGING PERMISSIONS]
+  // TODO: check if message is deleted by bot: causes a loop
+  // TODO: [REQUIERES MESSAGE MANAGING PERMISSIONS]
   // for each loop [
-  //  check if message excists: delete
   //  delete DB entry
   // ]
+  allMessageIDs.forEach((messageID) => {
+    // check if message excists: check channel permissions: delete
+    // TODO: check channel permissions
+  });
 };
 
 module.exports.help = {
