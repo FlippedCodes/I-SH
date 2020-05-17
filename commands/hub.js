c88b633a2d453b78ad1d0832767be9ee3a857308
Phil <philip.schaefer0@gmail.com>
2020-05-17 19:51:21 +0200

    activated comamnds and updated help

--- commands/hub.js
+++ commands/hub.js
@@ -12,7 +12,7 @@ function messageFail(client, message, body) {
 
 module.exports.run = async (client, message, args, config) => {
   const [subcmd] = args;
-  const commandValues = ['register', 'delete', 'join', 'leave', 'transfer'];
+  const commandValues = ['register', 'delete', 'join', 'leave', 'transfer', 'stats', 'rename'];
   if (commandValues.includes(subcmd)) {
     client.functions.get(`CMD_hub_${subcmd}`)
       .run(client, message, args, config);
@@ -25,6 +25,6 @@ module.exports.run = async (client, message, args, config) => {
 module.exports.help = {
   name: 'hub',
   title: 'Manage hub',
-  usage: 'register|delete|join|leave|transfer',
+  usage: 'register|delete|join|leave|transfer|stats|rename',
   desc: 'Manage hubs and links between channels.',
 };
