b8495916a863006129dca7ea42d3e56b09c08409
Phil <philip.schaefer0@gmail.com>
2020-05-17 19:50:19 +0200

    inital init

---
+++ functions/CMD_hub_ban.js
@@ -0,0 +1,9 @@
+// DISABLED: not done
+module.exports.run = async (client, message, args, config) => {
+  message.reply('ban');
+  // banns a channel from the hub
+};
+
+module.exports.help = {
+  name: 'CMD_hub_ban',
+};
