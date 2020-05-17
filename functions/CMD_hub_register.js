2dd848acd7bc6a3609430bb147d766b80a40a39d
Phil <philip.schaefer0@gmail.com>
2020-05-17 22:58:21 +0200

    removed typo

--- functions/CMD_hub_register.js
+++ functions/CMD_hub_register.js
@@ -36,7 +36,7 @@ module.exports.run = async (client, message, args, config) => {
   const fittedHubName = `${message.id}_${hubName}`;
   if (fittedHubName.length > 50) return messageFail(client, message, 'Sorry, your hubname ist too long... Try something shorter.');
   const created = await createHub(fittedHubName, message.author.id, config.maxAllowedHubs);
-  if (created) messageSuccess(client, message, `You created \`${hubName}\`! You can link with it by using \`${config.prefix}${module.exports.help.parent} join ${fittedHubName} [CHANNELID]\`.`);
+  if (created) messageSuccess(client, message, `You created \`${hubName}\`! You can link with it by using \`${config.prefix}${module.exports.help.parent} join ${fittedHubName}\`.`);
   else messageFail(client, message, `Your account already owns a maximum of ${config.maxAllowedHubs} hubs. You can't create more then that!`);
 };
 
