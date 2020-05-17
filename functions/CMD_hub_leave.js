089ee852f5788fc1277b61727d583fdb621164cb
Phil <philip.schaefer0@gmail.com>
2020-05-17 20:28:37 +0200

    reveresed code

--- functions/CMD_hub_leave.js
+++ functions/CMD_hub_leave.js
@@ -1,7 +1,5 @@
 const BridgedChannel = require('../database/models/bridgedChannel');
 
-const HubName = require('../database/models/hubName');
-
 const errHander = (err) => { console.error('ERROR:', err); };
 
 // creates a embed messagetemplate for succeded actions
@@ -16,56 +14,24 @@ function messageFail(client, message, body) {
     .run(client.user, message.channel, body, '', 16449540, false);
 }
 
-// gets hubID from DB
-async function getHubID(hubName) {
-  const result = await HubName.findOne({ attributes: ['hubID'], where: { hubName } }).catch(errHander);
-  if (result) return result.hubID;
-  return null;
-}
-
-// creates channel DB entry
-async function getBridgedChannelCount(hubID) {
-  const result = await BridgedChannel.findAndCountAll({ where: { hubID } }).catch(errHander);
-  if (!result) return null;
-  return result.count;
-}
-
-// creates channel DB entry
-async function createBridgedChannel(hubID, channelID) {
-  const result = await BridgedChannel.findOne({ where: { channelID } }).catch(errHander);
-  if (result) return false;
-  const [bridgedChannel] = await BridgedChannel.findOrCreate({
-    where: { channelID }, defaults: { hubID },
-  }).catch(errHander);
-  return true;
+// deletes channel entry from DB
+async function deleteChannel(channelID) {
+  const result = await BridgedChannel.destroy({ where: { channelID } }).catch(errHander);
+  return result;
 }
 
 module.exports.run = async (client, message, args, config) => {
   // TODO: check user permissions
   // Hold on! You dont have permissions to manage this channel. Try asking an admin to remove the link.
-  // get subcmd from args
-  const [subcmd, hubName] = args;
-  // check if hubname is present
-  if (!hubName) {
-    return messageFail(client, message,
-      `Command usage: 
-        \`\`\`${config.prefix}${module.exports.help.parent} ${subcmd} ${hubName || 'HUBNAME'}\`\`\``);
-  }
-  // get hubID
-  const hubID = await getHubID(hubName);
-  if (!hubID) return messageFail(client, message, `There is no hub named \`${hubName}\`! Try finding it by looking for one of the first messages in your channel with the discord search sorted by oldest message.`);
 
   // get custom channel
-  let channelID = message.channel.id;
-  if (customChnanelID) channelID = customChnanelID;
+  const channelID = message.channel.id;
 
-  const created = await createBridgedChannel(hubID, channelID);
+  const created = await deleteChannel(channelID);
   if (created) {
-    let successMessage = `This channel is now linked with \`${hubName}\``;
-    if (customChnanelID) successMessage = `The channel <#${channelID}> is now linked with \`${hubName}\``;
-    messageSuccess(client, message, successMessage);
+    messageSuccess(client, message, 'This channel is now not longer linked!');
   } else {
-    messageFail(client, message, `The channel you are trying to link is already linked with \`${hubName}\`! Try unlinking it by using \`${config.prefix}${module.exports.help.parent} leave\`.`);
+    messageFail(client, message, 'This channel is not linked with any hub! You can\'t unlink it.');
   }
 };
 
