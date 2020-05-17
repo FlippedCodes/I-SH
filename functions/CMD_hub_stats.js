a1f27dad9292d6e0fc8283d69c59af51a827b068
Phil <philip.schaefer0@gmail.com>
2020-05-17 19:31:22 +0200

    added note

--- functions/CMD_hub_stats.js
+++ functions/CMD_hub_stats.js
@@ -1,7 +1,8 @@
 module.exports.run = async (client, message, args, config) => {
   message.reply('stats');
+  // owner, channel age, name (, hubID)
 };
 
 module.exports.help = {
-  name: 'CMD_hub_transfer',
+  name: 'CMD_hub_stats',
 };
