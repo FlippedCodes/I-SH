051478bea320a34a4cf9f087f0a5d840f5c4208e
Phil <philip.schaefer0@gmail.com>
2020-05-24 12:37:41 +0200

    added color failsafe

--- commands/about.js
+++ commands/about.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 
 // creates a embed messagetemplate for succeded actions
 function postMessage(client, message, body, error) {
+  let color = 0;
+  if (message.channel.type !== 'dm') color = message.member.displayColor;
   client.functions.get('FUNC_richEmbedMessage')
-    .run(client.user, message.channel, body, 'About: ', message.member.displayColor, false);
+    .run(client.user, message.channel, body, 'About: ', color, false);
 }
 
 // creates a embed messagetemplate for failed text retrieving and posts error message into log
