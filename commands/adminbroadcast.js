592d17fb10efde44aa70a3bb83ab9a6b53650801
Phil <philip.schaefer0@gmail.com>
2020-09-21 00:46:31 +0200

    fixed newline bog

--- commands/adminbroadcast.js
+++ commands/adminbroadcast.js
@@ -6,7 +6,8 @@ const errHander = (err) => {
 
 module.exports.run = async (client, message, args, config) => {
   if (message.author.id !== '172031697355800577') return message.react('❌');
-  const body = `**[BROADCAST MESSAGE]**\n${args.join(' ')}`;
+  const splitMessage = message.content.split(' ').slice(1);
+  const body = `**[BROADCAST MESSAGE]**\n${splitMessage.join(' ')}`;
   const username = message.author.username;
   const avatarURL = message.author.avatarURL({ format: 'png', dynamic: true, size: 512 });
   const allHubChannels = await bridgedChannel.findAll({ attributes: ['channelID'] }).catch(errHander);
