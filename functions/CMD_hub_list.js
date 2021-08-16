4845cc0733d814dee8b6c2f9eb8296b81b829bc3
Phil <philip.schaefer0@gmail.com>
2021-08-16 21:11:03 +0200

    populated with code

--- functions/CMD_hub_list.js
+++ functions/CMD_hub_list.js
@@ -1,6 +1,34 @@
-// DISABLED: not done
+const HubName = require('../database/models/hubName');
+
+const errHander = (err) => { console.error('ERROR:', err); };
+
+// creates a embed messagetemplate for succeded actions
+function messageSuccess(client, message, body) {
+  client.functions.get('FUNC_MessageEmbedMessage')
+    .run(client.user, message.channel, body, '', 4296754, false);
+}
+
+// creates a embed messagetemplate for failed actions
+function messageFail(client, message, body) {
+  client.functions.get('FUNC_MessageEmbedMessage')
+    .run(client.user, message.channel, body, '', 16449540, false);
+}
+
+// gets hub names from DB
+async function getHubNames(ownerID) {
+  const result = await HubName.findAll({ attributes: ['hubName'], where: { ownerID } }).catch(errHander);
+  if (result) return result;
+  return null;
+}
+
 module.exports.run = async (client, message, args, config) => {
-  message.reply('lst');
+  // check if in DM
+  if (message.channel.type !== 'dm') return messageFail(client, message, 'This command can only be used in DM!');
+  // get hubNames
+  const hubNames = await getHubNames(message.author.id);
+  let names = '';
+  await hubNames.forEach((entry) => names += `${entry.hubName}\n`);
+  messageSuccess(client, message, `Your Hubs:\n\`\`\`\n${names}\`\`\``);
 };
 
 module.exports.help = {
