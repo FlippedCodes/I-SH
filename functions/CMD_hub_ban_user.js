f46069eba57f9e47e2a81d3c6df2c9ace2b6f720
Phil <philip.schaefer0@gmail.com>
2021-09-14 20:37:34 +0200

    added workflow

--- functions/CMD_hub_ban_user.js
+++ functions/CMD_hub_ban_user.js
@@ -11,6 +11,15 @@ module.exports.run = async (client, message, args, config) => {
   [subcmd, type, messageID].map((arg) => cutLength = cutLength + arg.length + 1);
   const slicedReason = await args.join(' ').slice(cutLength);
   messageSuccess(message, `\`\`\`${slicedReason}\`\`\``);
+  // check permissions
+  // get messageID => parse messageInstanceID
+  // get messageInstanceID => get channelID
+  // get channel from channelID and lookup messageID
+  // get hubID from channelID
+  // get author => parse userID
+  // check if owner: dont ban owner
+  // post bannedUserEntry [hubID + userID]
+  // update owner
 };
 
 module.exports.help = {
