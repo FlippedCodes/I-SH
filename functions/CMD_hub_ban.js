04845a673e676959ac21633448922486d3382445
Phil <philip.schaefer0@gmail.com>
2021-09-14 20:04:09 +0200

    populated with code

--- functions/CMD_hub_ban.js
+++ functions/CMD_hub_ban.js
@@ -1,9 +1,21 @@
-// DISABLED: not done
 module.exports.run = async (client, message, args, config) => {
-  message.reply('ban');
-  // banns a channel from the hub
+  // get subcmd from args
+  const [subcmd, type] = args;
+  // check provided values
+  switch (type) {
+    case 'user':
+      client.functions.get(`CMD_${module.exports.help.parent}_${subcmd}_${type}`)
+        .run(client, message, args, config);
+      return;
+
+    default:
+      return messageFail(client, message,
+        `Command usage: 
+        \`\`\`${config.prefix}${module.exports.help.parent} ${subcmd} ${type || 'user'} MESSAGEID REASON\`\`\``);
+  }
 };
 
 module.exports.help = {
   name: 'CMD_hub_ban',
+  parent: 'hub',
 };
