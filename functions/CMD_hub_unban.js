02f5a34c9f56c1b6a06f5aeb314f8aaf5294a58d
Phil <philip.schaefer0@gmail.com>
2021-09-14 20:37:26 +0200

    dummyfile

---
+++ functions/CMD_hub_unban.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, message, args, config) => {
+  // get subcmd from args
+  const [subcmd, type] = args;
+  // check provided values
+  switch (type) {
+    case 'user':
+      client.functions.get(`CMD_${module.exports.help.parent}_${subcmd}_${type}`)
+        .run(client, message, args, config);
+      return;
+
+    default:
+      return messageFail(message,
+        `Command usage: 
+        \`\`\`${config.prefix}${module.exports.help.parent} ${subcmd} ${type || 'user'} MESSAGEID REASON\`\`\``);
+  }
+};
+
+module.exports.help = {
+  name: 'CMD_hub_ban',
+  parent: 'hub',
+};
