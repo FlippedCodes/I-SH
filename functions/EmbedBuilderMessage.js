43aac2603d66a95987d025bac367cee2995a2ad9
Phil <philip.schaefer0@gmail.com>
2022-08-26 01:28:55 +0200

    [move] function rename

---
+++ functions/EmbedBuilderMessage.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports.run = async (interaction, body, title, color, footer, ephemeral) => {
+  // needs to be local as settings overlap from dofferent embed-requests
+  const embed = new EmbedBuilder();
+
+  if (body) embed.setDescription(body);
+  if (title) embed.setTitle(title);
+  if (color) embed.setColor(color);
+  if (footer) embed.setFooter({ text: footer });
+
+  const options = {
+    embeds: [embed],
+    components: [],
+    ephemeral: String(ephemeral) ? ephemeral : true,
+  };
+
+  return reply(interaction, options).catch(ERR);
+};
+
+module.exports.data = {
+  name: 'richEmbedMessage',
+};
