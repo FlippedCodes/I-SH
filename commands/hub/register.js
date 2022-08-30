cd7133c296c576f635f86c10b51fb63ead080914
Phil <philip.schaefer0@gmail.com>
2022-08-30 06:19:00 +0200

    [cleanup]

--- commands/hub/register.js
+++ commands/hub/register.js
@@ -10,12 +10,12 @@ module.exports.run = async (interaction, HubName, BridgedChannel, hubnameStr) =>
   const hubName = hubnameStr.replaceAll(' ', '_');
   const fittedHubName = `${interaction.id}_${hubName}`;
   if (fittedHubName.length > 50) return messageFail(interaction, 'Sorry, your hubname ist too long... Try something shorter.');
-  const created = await createHub(HubName, fittedHubName, interaction.user.id, config.maxAllowedHubs);
+  const created = await createHub(HubName, fittedHubName, interaction.user.id, config.commands.hub.maxAllowedHubs);
   if (created) {
     messageSuccess(interaction, `You created \`${hubName}\`!
       You can link with it by using \`/${interaction.commandName} join hubname: ${fittedHubName}\`.
       **Make sure you only give the hub name to servers that you want to invite.**`);
-  } else messageFail(interaction, `Your account already owns a maximum of ${config.maxAllowedHubs} hubs. You can't create more then that!`);
+  } else messageFail(interaction, `Your account already owns a maximum of ${config.commands.hub.maxAllowedHubs} hubs. You can't create more then that!`);
 };
 
 module.exports.data = { subcommand: true };
