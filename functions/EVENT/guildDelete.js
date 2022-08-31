f666404d4a207de06252977d9b9ef4374e3086de
Phil <philip.schaefer0@gmail.com>
2022-08-31 19:22:42 +0200

    [fix] blind fix for foreach function

--- functions/EVENT/guildDelete.js
+++ functions/EVENT/guildDelete.js
@@ -6,7 +6,7 @@ const MessageLink = require('../../database/models/messageLink');
 
 // Deletes all messages in every channel, if its deleted in one
 module.exports.run = async (guild) => {
-  guild.channels.forEach(async (channel) => {
+  guild.channels.cache.forEach(async (channel) => {
     if (!channel.type !== ChannelType.GuildText) return;
     const channelID = channel.id;
     const checkedChannel = await BridgedChannel.findOne({ attributes: ['channelID'], where: { channelID } }).catch(ERR);
