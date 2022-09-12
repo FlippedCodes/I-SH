ac0e295244fe615d00157447022b902954894e1e
Phil <philip.schaefer0@gmail.com>
2022-09-12 17:26:16 +0200

    [fix] speling mistake

--- functions/SETUP/status.js
+++ functions/SETUP/status.js
@@ -4,7 +4,7 @@ module.exports.run = async () => {
   // if (DEBUG) return;
   console.log(`[${module.exports.data.name}] Setting status...`);
   const servercount = await client.guilds.cache.size;
-  await client.user.setActivity(`for messages in ${servercount} servers!`, { type: ActivityType.Listening });
+  await client.user.setActivity(`messages in ${servercount} servers!`, { type: ActivityType.Listening });
   console.log(`[${module.exports.data.name}] Status set!`);
 };
 
