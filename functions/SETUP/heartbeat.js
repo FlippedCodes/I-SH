a9716d44c1f8195c47d3e3af325a85aa880fb21e
Phil <philip.schaefer0@gmail.com>
2022-09-12 17:24:30 +0200

    [fix] double runner

--- functions/SETUP/heartbeat.js
+++ functions/SETUP/heartbeat.js
@@ -1,6 +1,10 @@
+let running = false;
+
 // Calls all the functions that are needed for a heartbeat
 module.exports.run = async () => {
   if (DEBUG) return;
+  if (running) return;
+  running = true;
   console.log(`[${module.exports.data.name}] Start sending heartbeats...`);
   // botlists
   // TODO: Better implementation: call on startup or when servercount changes
