d933cb958fab2b5c91a0232ffe2ad2baa655b636
Phil <philip.schaefer0@gmail.com>
2022-12-20 14:20:10 +0100

    [remove] upgrade message is outdated

--- functions/EVENT/messageCreate.js
+++ functions/EVENT/messageCreate.js
@@ -33,35 +33,6 @@ module.exports.run = async (message) => {
   if (message.author.bot) return;
   if (message.channel.type === ChannelType.DM) return messageFail(interaction, 'Hello! Sorry, but this is for servers only.');
 
-  // function to to remind, that the bot upgraded
-  if (message.content.startsWith('+')) {
-    const userID = message.author.id;
-
-    if (await checkUser(userID)) return;
-
-    const defaultBody = 'Hi there! I have been upgraded to Slash-Commands (v.2.0.0) and no longer support the old prefix of `+`. Please use the new `/` instead!';
-
-    const confirmMessage = await message.reply({ embeds: [embed(defaultBody)], components: [buttons], fetchReply: true });
-    // For some reason that isnta-deletes the message?
-    // await sentMessage.delete({ timeout: 20000 });
-    // start button collector
-    const filter = (i) => userID === i.user.id;
-    const buttonCollector = confirmMessage.createMessageComponentCollector({ filter, time: 20000 });
-    buttonCollector.on('collect', async (used) => {
-      buttonCollector.stop();
-      if (used.customId === 'discard') {
-        await addUser(userID);
-        confirmMessage.edit({ embeds: [embed('Message discarded and won\'t be shown again for you.')], components: [] });
-        return;
-      }
-      return confirmMessage.edit({ embeds: [embed('Unknown response')], components: [] });
-    });
-    buttonCollector.on('end', async (collected) => {
-      if (collected.size === 0) confirmMessage.edit({ embeds: [embed(defaultBody)], components: [] });
-    });
-    return;
-  }
-
   client.functions.get('ENGINE_serverlink_sharedChannels').run(message);
 };
 
