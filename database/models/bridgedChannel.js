6818fe2af5032295fff8edb49ea69f0f510f1919
Phil <philip.schaefer0@gmail.com>
2023-01-17 16:43:39 +0100

    update - codestyle

--- database/models/bridgedChannel.js
+++ database/models/bridgedChannel.js
@@ -1,28 +1,31 @@
 const Sequelize = require('sequelize');
 
-module.exports = sequelize.define('bridgedChannel', {
-  channelID: {
-    type: Sequelize.STRING(30),
-    primaryKey: true,
-    unique: true,
-  },
-  serverID: {
-    type: Sequelize.STRING(30),
-    allowNull: false,
-  },
-  hubID: {
-    type: Sequelize.INTEGER(11),
-    allowNull: false,
-    references: {
-      model: 'hubnames',
-      key: 'hubID',
+module.exports = sequelize.define(
+  'bridgedChannel',
+  {
+    channelID: {
+      type: Sequelize.STRING(30),
+      primaryKey: true,
+      unique: true,
+    },
+    serverID: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+    },
+    hubID: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      references: {
+        model: 'hubnames',
+        key: 'hubID',
+      },
     },
   },
-},
-{
-  uniqueKeys: {
-    uniqueLink: {
-      fields: ['serverID', 'hubID'],
+  {
+    uniqueKeys: {
+      uniqueLink: {
+        fields: ['serverID', 'hubID'],
+      },
     },
   },
-});
+);
--- database/models/messageLink.js
+++ database/models/messageLink.js
@@ -1,23 +1,26 @@
 const Sequelize = require('sequelize');
 
-module.exports = sequelize.define('messageLink', {
-  messageInstanceID: {
-    type: Sequelize.STRING(30),
-    allowNull: false,
-  },
-  messageID: {
-    type: Sequelize.STRING(30),
-    primaryKey: true,
-  },
-  channelID: {
-    type: Sequelize.STRING(30),
-    allowNull: false,
-  },
-},
-{
-  uniqueKeys: {
-    messageUnique: {
-      fields: ['messageID', 'channelID'],
+module.exports = sequelize.define(
+  'messageLink',
+  {
+    messageInstanceID: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+    },
+    messageID: {
+      type: Sequelize.STRING(30),
+      primaryKey: true,
+    },
+    channelID: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
     },
   },
-});
+  {
+    uniqueKeys: {
+      messageUnique: {
+        fields: ['messageID', 'channelID'],
+      },
+    },
+  },
+);
