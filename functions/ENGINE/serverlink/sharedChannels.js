b760c5fc8643104da206d0913af7e72f37429771
Phil <philip.schaefer0@gmail.com>
2023-02-10 03:05:57 +0100

    add - invite support

--- functions/ENGINE/serverlink/sharedChannels.js
+++ functions/ENGINE/serverlink/sharedChannels.js
@@ -6,8 +6,7 @@ const bridgedChannel = require('../../../database/models/bridgedChannel');
 
 const MessageLink = require('../../../database/models/messageLink');
 
-const buttons = (servername) => new ActionRowBuilder()
-  // TODO: implement secound function, dont bother implementing into existing one
+const button = (servername) => new ActionRowBuilder()
   .addComponents([
     new ButtonBuilder()
       .setCustomId('servername')
@@ -16,22 +15,35 @@ const buttons = (servername) => new ActionRowBuilder()
       .setStyle(ButtonStyle.Secondary),
   ]);
 
+const buttonWithInvite = (servername, invite) => new ActionRowBuilder()
+  .addComponents([
+    new ButtonBuilder()
+      .setURL(invite.url)
+      .setLabel(servername.length >= 80 ? `${servername.slice(0, 76)}...` : servername)
+      .setStyle(ButtonStyle.Link),
+  ]);
+
 module.exports.run = async (message) => {
   // TODO: attach invite, if setting is on
   // get channelID
   const channelID = message.channel.id;
   // check if channelID is part of i-sh: get hubID
   // TODO: create channelcache for channels in list to reduce db calls
-  const sourceChannel = await bridgedChannel.findOne({ attributes: ['hubID'], where: { channelID } }).catch(ERR);
+  const sourceChannel = await bridgedChannel.findOne({ attributes: ['hubID', 'allowInvites'], where: { channelID } }).catch(ERR);
   if (!sourceChannel) return;
   // only remove old entries if new message gets sended to avoid DB overload
   client.functions.get('ENGINE_serverlink_messageGarbageCollection').run(config);
   const hubID = sourceChannel.hubID;
   // get all channels in hubID
-  const allHubChannels = await bridgedChannel.findAll({ attributes: ['channelID'], where: { hubID } }).catch(ERR);
+  const allHubChannels = await bridgedChannel.findAll({ attributes: ['channelID', 'allowInvites'], where: { hubID } }).catch(ERR);
   // create messageLink instance ID
   await MessageLink.create({ messageInstanceID: message.id, messageID: message.id, channelID: message.channel.id });
-  const predoneButtons = buttons(message.channel.guild.name);
+  // invite logic for source channel
+  const predoneButtons = button(message.channel.guild.name);
+  const predoneButtonsWInv = sourceChannel.allowInvites
+    // 1 week inv
+    ? buttonWithInvite(message.channel.guild.name, await message.channel.createInvite({ maxAge: 604800, unique: false }))
+    : predoneButtons;
   const username = message.author.username;
   const avatarURL = message.author.avatarURL({ format: 'png', dynamic: true, size: 512 });
   // lookup if existing attachment
@@ -47,7 +59,8 @@ module.exports.run = async (message) => {
     let hook = channelWebhooks.find((hook) => hook.owner.id === client.user.id);
     if (!hook) hook = await channel.createWebhook({ name: config.name }).catch(ERR);
     const sentMessage = await hook.send({
-      content: message.content, components: [predoneButtons], username, avatarURL, files,
+      // invite logic for remote channel
+      content: message.content, components: [postChannel.allowInvites ? predoneButtonsWInv : predoneButtons], username, avatarURL, files,
     }).catch(ERR);
     // create DB entry for messageLink
     MessageLink.create({ messageInstanceID: message.id, messageID: sentMessage.id, channelID: postChannelID });
