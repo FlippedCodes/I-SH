0929ba2ab038b4d12572b42b7b712591d050bea5
Phil <philip.schaefer0@gmail.com>
2023-02-10 04:43:32 +0100

    update - standartized join implementation

--- commands/hub/listservers.js
+++ commands/hub/listservers.js
@@ -4,9 +4,9 @@ module.exports.run = async (interaction, HubName, BridgedChannel) => {
   const hub = await HubName.findOne({ attributes: ['hubID'], where: { hubName: hubNameVal } }).catch(ERR);
   const allServers = await BridgedChannel.findAll({ attributes: ['serverID'], where: { hubID: hub.hubID } }).catch(ERR);
 
-  const serversFormatted = allServers.map((entry) => `• ${client.guilds.cache.get(entry.serverID).name || entry.serverID}`);
+  const serversFormatted = allServers.map((entry) => `• ${client.guilds.cache.get(entry.serverID).name || entry.serverID}`).join('\n');
   if (!allServers.length) return messageFail(interaction, 'There are no guilds in this hub.');
-  const desc = `**All ${allServers.length} guilds that are in this hub:**\n${serversFormatted.join('\n')}`;
+  const desc = `**All ${allServers.length} guilds that are in this hub:**\n${serversFormatted}`;
   return messageSuccess(interaction, desc.length >= 4000 ? `${desc.slice(0, 4000)}...\nAnd more...` : desc);
 };
 
