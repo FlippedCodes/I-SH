f1e938b97481309168adfbe7933c6057038d48d9
Phil <philip.schaefer0@gmail.com>
2023-02-13 01:55:41 +0100

    update - implemented

--- commands/blockuser/add.js
+++ commands/blockuser/add.js
@@ -1,21 +1,12 @@
-// creates channel DB entry
-async function createHub(HubName, name, ownerID, maxHubs) {
-  const result = await HubName.findAndCountAll({ where: { ownerID } }).catch(ERR);
-  if (result.count >= maxHubs) return false;
-  await HubName.create({ hubName: name, ownerID }).catch(ERR);
-  return true;
-}
 
-module.exports.run = async (interaction, HubName, BridgedChannel, hubnameStr) => {
-  const hubName = hubnameStr.replaceAll(' ', '_');
-  const fittedHubName = `${interaction.id}_${hubName}`;
-  if (fittedHubName.length > 50) return messageFail(interaction, 'Sorry, your hubname ist too long... Try something shorter.');
-  const created = await createHub(HubName, fittedHubName, interaction.user.id, config.commands.hub.maxAllowedHubs);
-  if (created) {
-    messageSuccess(interaction, `You created \`${hubName}\`!
-      You can link with it by using \`/${interaction.commandName} join hubname: ${fittedHubName}\`.
-      **Make sure you only give the hub name to servers that you want to invite.**`);
-  } else messageFail(interaction, `Your account already owns a maximum of ${config.commands.hub.maxAllowedHubs} hubs. You can't create more then that!`);
+module.exports.run = async (interaction, HubName, user, BlockedUser, hubIDRaw) => {
+  const reason = interaction.options.getString('reason', true);
+  const created = await BlockedUser.findOrCreate({
+    where: { hubID: hubIDRaw.hubID, userID: user.id, channelID: interaction.channel.id }, defaults: { reason },
+  }).catch(ERR);
+  if (created[1]) {
+    messageSuccess(interaction, `You blocked \`${user.tag}\` from messenging this server through this hub.`);
+  } else messageFail(interaction, `The user ${user.tag} is already blocked from from messenging this server through this hub.`);
 };
 
 module.exports.data = { subcommand: true };
