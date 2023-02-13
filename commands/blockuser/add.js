4b05cdab80a5176c813cebafa92adc96b08d0fbc
Phil <philip.schaefer0@gmail.com>
2023-02-13 16:59:26 +0100

    update - better wording

--- commands/blockuser/add.js
+++ commands/blockuser/add.js
@@ -4,8 +4,8 @@ module.exports.run = async (interaction, HubName, user, BlockedUser, hubIDRaw) =
     where: { hubID: hubIDRaw.hubID, userID: user.id, channelID: interaction.channel.id }, defaults: { reason },
   }).catch(ERR);
   if (created[1]) {
-    messageSuccess(interaction, `You blocked \`${user.tag}\` from messenging this server through this hub.`);
-  } else messageFail(interaction, `The user ${user.tag} is already blocked from from messenging this server through this hub.`);
+    messageSuccess(interaction, `You blocked \`${user.tag}\` from messenging this server through the hub.`);
+  } else messageFail(interaction, `The user \`${user.tag}\` is already blocked from from messenging this server through this hub.`);
 };
 
 module.exports.data = { subcommand: true };
