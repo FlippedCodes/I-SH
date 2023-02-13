57f6ed6120e103a84f571574e888cb5fa0f8bf25
Phil <philip.schaefer0@gmail.com>
2023-02-13 18:36:32 +0100

    update - added new line

--- commands/tos.js
+++ commands/tos.js
@@ -1,6 +1,6 @@
 module.exports.run = async (interaction) => {
   await interaction.deferReply({ ephemeral: true });
-  messageSuccess(interaction, 'Here is our ToS and privacy policy: https://github.com/FlippedCodes/I-SH/wiki/ToS-and-Privacy-Policy');
+  messageSuccess(interaction, 'Here is our ToS and privacy policy:\nhttps://github.com/FlippedCodes/I-SH/wiki/ToS-and-Privacy-Policy');
 };
 
 module.exports.data = new CmdBuilder()
