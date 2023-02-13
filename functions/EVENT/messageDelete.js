3ed54ee8cb6b818e3568125cebc34b9c52de56cf
Phil <philip.schaefer0@gmail.com>
2023-02-13 02:04:12 +0100

    update - cleanup

--- functions/EVENT/messageDelete.js
+++ functions/EVENT/messageDelete.js
@@ -4,49 +4,28 @@ const BridgedChannel = require('../../database/models/bridgedChannel');
 
 const MessageLink = require('../../database/models/messageLink');
 
-async function checkChannel(channelID) {
-  const result = await BridgedChannel.findOne({ attributes: ['channelID'], where: { channelID } }).catch(ERR);
-  return result;
-}
-
-async function getMessageInstance(messageID) {
-  const result = await MessageLink.findOne({ attributes: ['messageInstanceID'], where: { messageID } }).catch(ERR);
-  return result;
-}
-
-async function getDBMessages(messageInstanceID) {
-  const result = await MessageLink.findAll({ attributes: ['messageID', 'channelID'], where: { messageInstanceID } }).catch(ERR);
-  return result;
-}
-
-function deleteDBMessage(messageInstanceID) {
-  MessageLink.destroy({ where: { messageInstanceID } });
-}
-
 async function getMessages(messageID) {
   // check DB for messageID and get messageInstanceID
-  const messageInstanceID = await getMessageInstance(messageID);
+  const messageInstanceID = await MessageLink.findOne({ attributes: ['messageInstanceID'], where: { messageID } }).catch(ERR);
   if (!messageInstanceID) return null;
   // get all messageIDs
   const coreID = messageInstanceID.messageInstanceID;
-  const allMessageIDs = await getDBMessages(coreID);
-  deleteDBMessage(coreID);
+  const allMessageIDs = await MessageLink.findAll({ attributes: ['messageID', 'channelID'], where: { messageInstanceID: coreID } }).catch(ERR);
+  MessageLink.destroy({ where: { messageInstanceID: coreID } });
   return allMessageIDs;
 }
 
 // Deletes all messages in every channel, if its deleted in one
 module.exports.run = async (message) => {
   // check if channel is part of service
-  if (!await checkChannel(message.channel_id)) return;
+  if (!await BridgedChannel.findOne({ attributes: ['channelID'], where: { channelID: message.channelId } }).catch(ERR)) return;
 
   const allMessageIDs = await getMessages(message.id);
   if (!allMessageIDs) return;
 
   allMessageIDs.forEach(async (entry) => {
     if (message.id === entry.messageID) return;
-    const channel = await client.channels.cache.find((channel) => channel.id === entry.channelID);
-    const targetMessage = await channel.messages.fetch(entry.messageID);
-    if (targetMessage.deleted) return;
+    const channel = await client.channels.cache.get(entry.channelID);
     if (!channel.guild.members.me.permissionsIn(channel).has(PermissionsBitField.Flags.ManageMessages)) return;
     targetMessage.delete();
   });
