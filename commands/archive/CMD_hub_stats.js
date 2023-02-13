eeff8a6cc49979854568ef1a42fdfeac3df748cf
Phil <philip.schaefer0@gmail.com>
2023-02-13 17:06:51 +0100

    remove - not needed anymore

--- commands/archive/CMD_hub_stats.js
+++
@@ -1,9 +0,0 @@
-// DISABLED: not done
-module.exports.run = async (client, message, args, config) => {
-  message.reply('stats');
-  // owner, channel age, channelAmmount, name (, hubID)
-};
-
-module.exports.help = {
-  name: 'CMD_hub_stats',
-};
