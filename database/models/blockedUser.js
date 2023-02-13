6eece68835c4c7633c40932b39944fb485775f36
Phil <philip.schaefer0@gmail.com>
2023-02-13 04:08:52 +0100

    update - update db model

--- database/models/blockedUser.js
+++ database/models/blockedUser.js
@@ -1,21 +1,36 @@
 const Sequelize = require('sequelize');
 
 module.exports = sequelize.define('blockedUser', {
+  blockID: {
+    type: Sequelize.INTEGER(11),
+    primaryKey: true,
+    autoIncrement: true,
+  },
   userID: {
     type: Sequelize.STRING(30),
-    primaryKey: true,
+    allowNull: false,
+  },
+  channelID: {
+    type: Sequelize.STRING(30),
+    allowNull: false,
   },
   hubID: {
     type: Sequelize.INTEGER(11),
     allowNull: false,
   },
   reason: {
-    type: Sequelize.TEXT,
+    type: Sequelize.TEXT('tiny'),
     allowNull: false,
   },
   acknowledged: {
     type: Sequelize.BOOLEAN,
     allowNull: false,
     defaultValue: false,
+  },
+}, {
+  uniqueKeys: {
+    blockUnique: {
+      fields: ['userID', 'channelID', 'hubID'],
+    },
   },
 });
