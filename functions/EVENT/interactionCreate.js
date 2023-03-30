70901c5d4ca5c7ae6595492cb642021eb4c79d8e
Phil <philip.schaefer0@gmail.com>
2023-03-31 00:11:56 +0200

    update - comment

--- functions/EVENT/interactionCreate.js
+++ functions/EVENT/interactionCreate.js
@@ -1,9 +1,9 @@
 module.exports.run = async (interaction) => {
   // only guild command
-  // TODO: implement command permission system, that way permission checking and dm checking is not nessesary anymore.
+  // TODO: implement command permission system, that way permission checking and dm checking is not necessary anymore.
   if (!await interaction.inGuild()) return messageFail(interaction, 'The bot is for server-use only.');
 
-  // autocomplete hanlder
+  // autocomplete handler
   if (interaction.isAutocomplete()) return client.functions.get('EVENT_interaction_isAutocomplete').run(interaction).catch(ERR);
   // command handler
   if (interaction.isCommand()) return client.functions.get('EVENT_interaction_isCommand').run(interaction).catch(ERR);
